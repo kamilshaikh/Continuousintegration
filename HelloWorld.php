230949448b333eb3c97b1d63d47054b5d26fd98f
kamil <kamils@sfwltd.co.uk>
2017-06-19 14:13:48 +0530

    test build

--- HelloWorld.php
+++ HelloWorld.php
@@ -14,7 +14,7 @@ class HelloWorld
 
     public function hello($what = 'World')
     {
-        $sql = "INSERT INTO hello1 VALUES (" . $this->pdo->quote($what) . ")";
+        $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
         $this->pdo->query($sql);
         return "Hello $what";
     }
